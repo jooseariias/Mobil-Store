21b71047536515bbf4ce47a74657dc88e0850c55
facundo-maciel <maci.facundo@gmail.com>
2023-03-29 00:43:44 -0300

    redux casi listo

---
+++ client/src/redux/actions/index.js
@@ -0,0 +1,73 @@
+import axios from "axios";
+
+export const GET_PHONES = "GET_PHONES";
+export const GET_DETAIL = "GET_DETAIL";
+export const GET_BRANDS = "GET_BRANDS";
+export const POST_PHONE = "POST_PHONE";
+export const ORDER_BY_NAME = "ORDER_BY_NAME";
+export const ORDER_BY_RELEASED = "ORDER_BY_RELEASED";
+export const TIDY_PRICE = "TIDY_PRICE";
+export const FILTER_BRANDS = "FILTER_BRANDS";
+
+export function getPhones(){
+    return async function (dispatch) {
+      let Json = await axios.get(`http://localhost:3001/product`);
+      dispatch({
+        type: GET_PHONES,
+        payload: Json.data,
+      });
+    }
+  }
+
+  export function getDetail(id) {
+    return async function(dispatch) {
+        const json = await axios(`http://localhost:3001/product/${id}`);
+        return dispatch({
+            type: GET_DETAIL,
+            payload: json.data
+        });
+    };
+  };
+
+  export function getBrands() {
+    return async function (dispatch) {
+      let json = await axios.get(`http://localhost:3001/brand`);
+      dispatch({
+        type: GET_BRANDS,
+        payload: json.data,
+      });
+    };
+  }
+
+  export function PostPhone(payload) {
+    var json = axios.post(`http://localhost:3001/product`, payload);
+    return { type: POST_PHONE, payload: json };
+  }
+
+  export function TidyAlphabetically(payload) {
+    return {
+      type: ORDER_BY_NAME,
+      payload,
+    };
+  }
+
+  export function TidyReleased(payload) {
+    return {
+      type: ORDER_BY_RELEASED,
+      payload,
+    };
+  }
+
+  export function TidyPrice(payload) {
+    return {
+      type: TIDY_PRICE,
+      payload,
+    };
+  }
+  
+  export function FilterBrands(payload) {
+    return {
+      type: FILTER_BRANDS,
+      payload,
+    };
+  }
---
+++ client/src/redux/reducer/index.js
@@ -0,0 +1,160 @@
+import { Action } from "@remix-run/router";
+import{
+    GET_PHONES, 
+    GET_DETAIL,
+    GET_BRANDS,
+    POST_PHONE,
+    ORDER_BY_NAME,
+    ORDER_BY_RELEASED,
+    TIDY_PRICE,
+    FILTER_BRANDS
+}from"../actions/index"
+
+const initialState = {
+    Phones: [],
+    PhonesCopy: [],
+    Brands: [],
+    details: []
+};
+
+function rootReducer(state = initialState, action) {
+    switch (action.type) {
+  
+      case GET_PHONES:
+        return {
+          ...state,
+          Phones: action.payload,
+          PhonesCopy: action.payload,
+        };
+  
+      case GET_DETAIL:
+        return{
+          ...state,
+          details: action.payload
+        }
+  
+  
+    //   case POST_PHONE:
+    //     return {
+    //       ...state,
+    //       Phones: action.payload,
+    //       PhonesCopy: action.payload,
+    //     };
+  
+  
+      case GET_BRANDS:
+        return {
+          ...state,
+          Brands: action.payload,
+        };
+  
+ 
+  
+      case POST_PHONE:
+        return {
+          ...state,
+        };
+  
+      //reducers de ordenamiento
+  
+      case ORDER_BY_NAME:
+        console.log("1");
+        let order =
+          action.payload === "asc"
+            ? state.Phones.sort(function (a, b) {
+                if (a.name.toLowerCase() > b.name.toLowerCase()) {
+                  return 1;
+                }
+                if (b.name.toLowerCase() > a.name.toLowerCase()) {
+                  return -1;
+                }
+                return 0;
+              })
+            : state.Phones.sort(function (a, b) {
+                if (a.name.toLowerCase() > b.name.toLowerCase()) {
+                  return -1;
+                }
+                if (b.name.toLowerCase() > a.name.toLowerCase()) {
+                  return 1;
+                }
+                return 0;
+              });
+        return {
+          ...state,
+          PhonesCopy: order,
+        };
+  
+      case ORDER_BY_RELEASED:
+        let orderByReleased =
+          action.payload === "asc"
+            ? state.Phones.sort(function (a, b) {
+                if (a.year.toLowerCase() > b.year.toLowerCase()) {
+                  return 1;
+                }
+                if (b.year.toLowerCase() > a.year.toLowerCase()) {
+                  return -1;
+                }
+                return 0;
+              })
+            : state.Phones.sort(function (a, b) {
+                if (a.year.toLowerCase() > b.year.toLowerCase()) {
+                  return -1;
+                }
+                if (b.year.toLowerCase() > a.year.toLowerCase()) {
+                  return 1;
+                }
+                return 0;
+              });
+        return {
+          ...state,
+          PhonesCopy: orderByReleased,
+        };
+  
+      case TIDY_PRICE:
+        let TidyPrice =
+          action.payload === "min"
+            ? state.Phones.sort(function (a, b) {
+                if (a.price > b.price) {
+                  return 1;
+                }
+                if (b.price > a.price) {
+                  return -1;
+                }
+                return 0;
+              })
+            : state.Phones.sort(function (a, b) {
+                if (a.price > b.price) {
+                  return -1;
+                }
+                if (b.price > a.price) {
+                  return 1;
+                }
+                return 0;
+              });
+        return {
+          ...state,
+          PhonesCopy: TidyPrice,
+        };
+  
+      //reduces de filtrados
+  
+  
+      case FILTER_BRANDS:
+        const Allbran = state.PhonesCopy;
+        const TypePhonesFilter =
+          action.payload === "all"
+            ? Allbran
+            : Allbran?.filter((t) => t.brand.includes(action.payload));
+        return {
+          ...state,
+          PhonesCopy: TypePhonesFilter,
+        };
+
+  
+      default: {
+        return state;
+      }
+    }
+  }
+  
+  export default rootReducer;
