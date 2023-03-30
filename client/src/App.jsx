9c56da483b7280b0e078c55203abd214f3288421
Victor Pestana <60305666+stringVictorinox@users.noreply.github.com>
2023-03-30 12:12:12 -0400

    creación del footer y header

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,14 @@
-export default function App() {
+import { Route, Routes } from "react-router-dom"
+import Home from "./views/Home/Home";
+
+export default function App(){
+  
+
   return (
     <div className="App">
-      <h1 className="text-purple-400 text-2xl">Hello world</h1>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+      </Routes>
     </div>
   );
 }
---
+++ client/src/views/Home/Home.jsx
@@ -0,0 +1,11 @@
+import Header from "../Header/Header"
+import Footer from "../Footer/Footer"
+
+export default function Home() {
+  return (
+    <div>
+      <Header />
+      <Footer />
+    </div>
+  )
+}
