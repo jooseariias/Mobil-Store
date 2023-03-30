442f2262be1f3eceb84d57e790d29ef1d198214c
Victor Pestana <60305666+stringVictorinox@users.noreply.github.com>
2023-03-30 14:24:12 -0400

    footer

---
+++ client/src/assets/icons-header/icons.js
@@ -0,0 +1,11 @@
+import carrito from './carrito.png'
+import logo from './logo.png'
+import favoritos from './favoritos.png'
+import usuario from './usuario.png'
+
+export default {
+    "logo": logo,
+    "carrito": carrito,
+    "favoritos": favoritos,
+    "usuario": usuario,
+}
