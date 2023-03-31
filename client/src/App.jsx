175a339f75ea0f5528e6eda27312a2a2d3aa8361
JKaypa <jose_kaypa@hotmail.com>
2023-03-30 20:25:42 -0500

    Add details

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,19 +1,23 @@
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
+import { Route, Routes } from "react-router-dom";
+import Details from "./components/details/details";
 import Home from "./components/home/home";
 import { getPhones } from "./redux/actions";
 
 export default function App() {
-
   const dispatch = useDispatch();
 
   useEffect(() => {
-    dispatch(getPhones())
-  }, [])
+    dispatch(getPhones());
+  }, []);
 
   return (
     <div>
-      <Home />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="details/:id" element={<Details />} />
+      </Routes>
     </div>
   );
 }
