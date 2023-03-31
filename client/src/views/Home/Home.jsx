d914e17ae7e13b6f93f5ac3d0dcad2903c496343
Victor Pestana <60305666+stringVictorinox@users.noreply.github.com>
2023-03-30 20:07:57 -0400

    fix: cards

--- client/src/views/Home/Home.jsx
+++ client/src/views/Home/Home.jsx
@@ -1,11 +1,17 @@
 import Header from "../../components/Header/Header"
 import Carrucel from "../../components/Carrucel/Carrucel"
+import Cards from "../../components/cards/cards"
 
 export default function Home(){
   return (
     <div>
       <Header />
       <Carrucel />
+
+      <div>
+        <Cards />
+      </div>
+
     </div>
   )
 }
