81ce0c7fcd521cf8331eaa4b07855046a3ce2179
facundo-maciel <maci.facundo@gmail.com>
2023-04-14 22:04:14 -0300

    cambios

--- client/src/views/Dashboard/Users/User.jsx
+++ client/src/views/Dashboard/Users/User.jsx
@@ -13,11 +13,6 @@ export default function UsersDashBoard(){
     const dispatch = useDispatch();
     const allUsers = useSelector((state) => state.Users);
 
-    // const [start, setStart] = useState(0);
-    // const [currentPage, setCurrentPage] = useState(1);
-    // const quantity = 9;
-    // const usersDisplayed = allUsers.slice(start, start + quantity );
-
     useEffect(() => {
         dispatch(GetUsers());
       }, [dispatch]); 
@@ -36,18 +31,6 @@ export default function UsersDashBoard(){
 
                 <div class='flex text-center bg-white items-center justify-center mx-80 flex-wrap w-[1000px] px-40'>
              
-                    {/* {usersDisplayed?.map((e) => {
-                    return (
-                        <CardDashBoard
-                        key={e.id}
-                        name={e.name}
-                        surname={e.surname}
-                        email={e.email}
-                        rol={e.rol}
-                        enabled={e.enabled}
-                        />
-                        );
-                    })} */}
                     <CardDashBoard/>
 
                 </div>
