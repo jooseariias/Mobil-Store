e1ad118bede91c346cfb51c084f19c73b757d2d9
facundo-maciel <maci.facundo@gmail.com>
2023-04-19 11:24:17 -0300

    me traigo los cambios

--- client/src/views/Dashboard/Dashboard.jsx
+++ client/src/views/Dashboard/Dashboard.jsx
@@ -38,7 +38,6 @@ export default function Dashboard(){
                                 {allStatisticsTotal !== null ? ( <h1 className="w-[400px] font-bold text-2xl px-10 py-4 border border-gray-600 mx-8 h-[170px] rounded-xl my-8 border-4 bg-blue-100 pt-8"> The amount of sales in the last year is: <h2 className="font-bold text-green-500 text-2xl ">{allStatisticsTotal.count}</h2> </h1>  )   : ( <h1>indefined count  </h1>) } 
                                 {allStatisticsTotal !== null ? ( <h1 className=" w-[400px] font-bold text-2xl px-10 py-4 border border-gray-600 mx-8 h-[170px] rounded-xl my-8 border-4 bg-blue-100 pt-8"> and the total earned on sales is: <h2 className="font-bold text-green-500 text-2xl">{allStatisticsTotal.total}</h2> </h1>  )   : ( <h1> indefined count total  </h1>) } 
                                 </div>
-
                     </div>
 
                     <div class='flex text-center mx-[30px] mt-[20px] bg-white mb-[20px] p-[10px  justify-center items-center  w-[1000px] h-[200px]  px-40'>
