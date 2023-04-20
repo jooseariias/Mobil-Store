ec62c42cbe2fc5b768437e2dfed2491fa48493a5
Victor Pestana <60305666+stringVictorinox@users.noreply.github.com>
2023-04-19 22:33:01 -0400

    fix: login

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -115,7 +115,7 @@ export default function Header(){
             ) : (
               <Link to={"/Profile"}>
                 <img
-                  src={user.data_user.image}
+                  src={user?.data_user.image}
                   alt=""
                   className="hover:transform hover:scale-110 w-7 h-7 rounded-full mt-1"
                 />
